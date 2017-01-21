9aab4c3e7f7c7d241502f6d8ca3d794adf8b3d8d
Pavol Polacek <ejko@seznam.cz>
2017-01-21 16:07:35 +0100

    Splashscreen and Appicons

--- tripinstagram/SplashVC.swift
+++ tripinstagram/SplashVC.swift
@@ -3,33 +3,42 @@
 //  tripinstagram
 //
 //  Created by Pavol Polacek on 05/01/2016.
-//  Copyright © 2016 Pavol Polacek. All rights reserved.
+//  Copyright © 2017 Pavol Polacek. All rights reserved.
 //
 
 import UIKit
 
-class splashVC: UIViewController {
+import UIKit
 
+/**
+ * Snippet from CodeTrench.com
+ */
+class SplashVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         addBackgroundImage()
         addLogo()
         
-        // Show the home screen after a bit. Calls the show() function.
-        _ = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(showapp), userInfo: nil, repeats: true)
+        // Show the home screen after a bit. Calls the showSplash() function.
+        _ = Timer.scheduledTimer(
+            timeInterval: 2.5, target: self, selector: #selector(showSplash), userInfo: nil, repeats: false
+        )
     }
     
     /*
      * Gets rid of the status bar
      */
-
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
     /*
      * Shows the app's main home screen.
      * Gets called by the timer in viewDidLoad()
      */
-    func showapp() {
+    func showSplash() {
         self.performSegue(withIdentifier: "showApp", sender: self)
     }
     
@@ -39,7 +48,7 @@ class splashVC: UIViewController {
     func addBackgroundImage() {
         let screenSize: CGRect = UIScreen.main.bounds
         
-        let bg = UIImage(named: "bg.png")
+        let bg = UIImage(named: "launch.png")
         let bgView = UIImageView(image: bg)
         
         bgView.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
@@ -52,13 +61,13 @@ class splashVC: UIViewController {
     func addLogo() {
         let screenSize: CGRect = UIScreen.main.bounds
         
-        let logo     = UIImage(named: "like.png")  // logo.png
+        let logo     = UIImage(named: "Postrip.png")
         let logoView = UIImageView(image: logo)
         
         let w = logo?.size.width
         let h = logo?.size.height
         
-        logoView.frame = CGRect(x: (screenSize.width/2) - (w!/2), y: 5, width: w!, height: h! )
+        logoView.frame = CGRect(x: (screenSize.width/2) - (w!/2), y: 5, width: w!, height: h!)
         self.view.addSubview(logoView)
     }
 }
